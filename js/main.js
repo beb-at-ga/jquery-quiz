b20ad522c4fda5e69f283697a64659c1532e3984
Bryan Brinson <bryan@beb.com>
2019-08-01 08:45:04 -0700

    updated some styling (visual and code)

--- js/main.js
+++ js/main.js
@@ -1,157 +1,102 @@
 let questions = [{
-    prompt: "Question 1: What is the name of his dog?",
-    answers: ["Justin", "Wayne", "Tim", "Mike"],
-    correctAnswerIndex: 0
-},
-{
-    prompt: "Question 2: What is the name of his cat?",
-    answers: ["Caesar", "Milo", "Rocky", "Sox"],
-    correctAnswerIndex: 2
-},
-{
-    prompt: "Question 3: What is the mother\'s name?",
-    answers: ["Martha", "Mary", "Teresa", "Val"],
-    correctAnswerIndex: 3
-},
-{
-    prompt: "Question 4: What is the Father\'s name?",
-    answers: ["Don", "John", "Jim", "Bill"],
-    correctAnswerIndex: 3
-},
-{
-    prompt: "Question 5: What is the child\'s name?",
-    answers: ["Sam", "Martin", "Mike", "Goober"],
-    correctAnswerIndex: 4
-}
+        prompt: "Question 1: What is the name of his dog?",
+        answers: ["Justin", "Wayne", "Tim", "Mike"],
+        correctAnswer: "Justin"
+    },
+    {
+        prompt: "Question 2: What is the name of his cat?",
+        answers: ["Caesar", "Milo", "Rocky", "Sox"],
+        correctAnswer: "Caesar"
+    },
+    {
+        prompt: "Question 3: What is the mother\'s name?",
+        answers: ["Martha", "Mary", "Teresa", "Val"],
+        correctAnswer: "Martha"
+    },
+    {
+        prompt: "Question 4: What is the Father\'s name?",
+        answers: ["Don", "John", "Jim", "Bill"],
+        correctAnswer: "Don"
+    },
+    {
+        prompt: "Question 5: What is the child\'s name?",
+        answers: ["Sam", "Martin", "Mike", "Goober"],
+        correctAnswer: "Sam"
+    }
 ];
 
 
 let userAnswers = [];
-var arrAnswers = ["Justin", "Milo", "Mary", "Jim", "Mike"]
+let numberCorrect = 0;
+let clickCount = 0;
 
 $(document).ready(function () {
-    // do some init
-    $("#submit").attr("disabled", true);
-    console.log(questions.length);
-
+    $("#next").attr("disabled", true);
+    $("#prev").attr("disabled", true);
     displayQuestion(questions);
 
 });
 
-var numberCorrect = 0
-var clickCount = 0
-
-// clickCount.clamp(clickCount, 0, 4);
-
 $('.button').on('click', function (event) {
-    console.log('Click Count', clickCount)
-    // if (clickCount >= 0 && clickCount < 4) {
-    if (event.target.value == "Submit" || event.target.value == "Next") {
-        if (clickCount >= 0 && clickCount < 5) {
-            
-            clickCount++
-            
-            console.log(event.target, 'asdfasdf');
-            console.log($("input:checked + label").text(), 'asldjfalsdf');
-            
-            userAnswers.push($("input:checked + label").text()); //This is the correct way to push the selected value to userAnswers array
-            console.log(userAnswers);
-            displayQuestion(questions);
+    if (event.target.value === "Submit" || event.target.value === "Next") {
+        if (clickCount >= 0 && clickCount <= 4) {
+            $("#prev").attr("disabled", false);
+
+            clickCount++;
+            //This is the correct way to push the selected value to userAnswers array
+            userAnswers.push($("input:checked + label").text());
             // console.log(checkedButton); //How come if I comment this out it jumps to line 72?
-            console.log('asdfhskdjhfkjasdfkjasdf')
-            
-            
         }
-        if (clickCount == 5) { //if you are on final page and click next or submit
-            
 
-            for (var i = 0; i < arrAnswers.length; i++) {
-                if (arrAnswers[i] === userAnswers[i]) {
-                    numberCorrect += 1
-                } 
-            } finalScore();
-           
-        }
-    }
-    if (event.target.value == "Previous") {
-        if (clickCount > 0 && clickCount <= 4) {
-            clickCount--
+        //if you are on final page and click next or submit
+        if (clickCount === 5) {
+            for (let i = 0; i < questions.length; i++) {
+                if (questions[i].correctAnswer === userAnswers[i]) {
+                    numberCorrect += 1;
+                }
+            }
+            finalScore();
+        } else {
             displayQuestion(questions);
-            // let prevAnswer = userAnswers.pop();
-            console.log(userAnswers);
-            $('form input[type="radio"]').prop("checked", true);
-            // $('form[name="foo"]').show();
-            // $(".button").show();
         }
     }
-})
-// else {
-//     alert('Toooo Far')
-// }
-// })
-
+    if (event.target.value === "Previous") {
+        if (clickCount > 0 && clickCount <= 4) {
+            clickCount--;
+            displayQuestion(questions);
+            $('form input[type="radio"]').prop("checked", true);
+        }
+    }
+});
 
 let displayQuestion = function (questions) {
-
     $('#question').html(questions[clickCount].prompt);
 
     for (let i = 0; i < questions[clickCount].answers.length; i++) {
-        // console.log(questions[clickCount].answers[i]);
         $(`label[for='${i}']`).html(questions[clickCount].answers[i]);
-        // $(`a${i}`).data("answerIndex", questions[clickCount].answers[i]);
     }
 
-    disableSubmit();
+    $("#next").attr("disabled", true);
 
     $('input').on('click', function () {
-
-        enableButton();
-
+        $("#next").attr("disabled", false);
     });
-
-
-}
-
-
-// document.getElementById("submit").addEventListener("click", disableSubmit); // Runs the function when a clicked
-// $('#submit').on('click', function (click) {
-//     // function disableSubmit(click){
-//     if (document.getElementById('a1').checked) {
-//         const disabledSubmit = click.target;
-//         disabledSubmit.disabled = true;
-//     } else {
-//         alert('Please Select One Option!')
-//     }
-// });
-
-function disableSubmit(submit) {
-    // const disabledSubmit = click.target;
-    $("#submit").attr("disabled", true);
-    //         disabledSubmit.disabled = true;
-}
-
-function enableButton() {
-    // document.getElementById("submit").disabled = false;
-    $("#submit").attr("disabled", false);
 }
 
 function finalScore() {
-    console.log('You got', numberCorrect, 'out of 5 correct!')
-    document.getElementById('question').innerText = ('You got' + ' ' + numberCorrect + ' ' + 'out of 5 correct!')
-    // document.getElementById("element").style.display = "none";
-    $('form[name="foo"]').hide();
+    document.getElementById('question').innerText = (`You got ${numberCorrect} out of ${questions.length}`);
+
+    $('form[name="answer-form"]').hide();
     $(".button").hide();
-    // ('You got', numberCorrect, 'out of 5 correct!')
-    var button = document.createElement("button");
-button.innerHTML = "Play Again?";
-
-// 2. Append somewhere
-var body = document.getElementsByTagName("body")[0];
-body.appendChild(button);
-
-// 3. Add event handler
-button.addEventListener ("click", function() {
-    location.reload();
-});
     
-}
+    var button = document.createElement("button");
+    button.innerHTML = "Play Again?";
+    button.classList.add("button");
+
+    $("main").append(button);
+
+    // 3. Add event handler 
+    button.addEventListener("click", function () {
+        location.reload();
+    });
+}   
