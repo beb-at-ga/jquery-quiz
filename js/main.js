1cad1b630ceee07466cd663ded78480f80281dd5
Bryan Brinson <bryan@beb.com>
2019-07-31 22:29:33 -0700

    major updates

--- js/main.js
+++ js/main.js
@@ -1,33 +1,33 @@
 let questions = [{
-        prompt: "Question 1 ",
-        answers: ["Justin", "Wayne", "Tim", "Mike"],
-        correctAnswerIndex: 0
-    },
-    {
-        prompt: "Question 2",
-        answers: ["dsfsd", "Rub4324234y", "hfghfghfg", "sfdgdfg"],
-        correctAnswerIndex: 2
-    },
-    {
-        prompt: "Question 3",
-        answers: ["gsdghjju6", "gfsdgs", "vxfgdf", "jyjxfh"],
-        correctAnswerIndex: 3
-    },
-    {
-        prompt: "Question 4",
-        answers: ["345t345t", "4534t", "534t34", "g3q345ger"],
-        correctAnswerIndex: 3
-    },
-    {
-        prompt: "Question 5",
-        answers: ["frefwer", "fhhfds", "r4q3fq34", "Pytht3t3t43on"],
-        correctAnswerIndex: 4
-    }
+    prompt: "Question 1: What is the name of his dog?",
+    answers: ["Justin", "Wayne", "Tim", "Mike"],
+    correctAnswerIndex: 0
+},
+{
+    prompt: "Question 2: What is the name of his cat?",
+    answers: ["Caesar", "Milo", "Rocky", "Sox"],
+    correctAnswerIndex: 2
+},
+{
+    prompt: "Question 3: What is the mother\'s name?",
+    answers: ["Martha", "Mary", "Teresa", "Val"],
+    correctAnswerIndex: 3
+},
+{
+    prompt: "Question 4: What is the Father\'s name?",
+    answers: ["Don", "John", "Jim", "Bill"],
+    correctAnswerIndex: 3
+},
+{
+    prompt: "Question 5: What is the child\'s name?",
+    answers: ["Sam", "Martin", "Mike", "Goober"],
+    correctAnswerIndex: 4
+}
 ];
 
 
 let userAnswers = [];
-
+var arrAnswers = ["Justin", "Milo", "Mary", "Jim", "Mike"]
 
 $(document).ready(function () {
     // do some init
@@ -38,23 +38,39 @@ $(document).ready(function () {
 
 });
 
-
+var numberCorrect = 0
 var clickCount = 0
+
 // clickCount.clamp(clickCount, 0, 4);
 
 $('.button').on('click', function (event) {
-
+    console.log('Click Count', clickCount)
     // if (clickCount >= 0 && clickCount < 4) {
     if (event.target.value == "Submit" || event.target.value == "Next") {
-        if (clickCount >= 0 && clickCount < 4) {
+        if (clickCount >= 0 && clickCount < 5) {
+            
             clickCount++
-            displayQuestion(questions);
-            console.log(event.target);
-            // let checkedButton = $('input[name="answer-options"]:checked').val();
-            // console.log(checkedButton);
-            userAnswers.push($(checkedButton));
-            $('form input[type="radio"]').prop("checked", false);
+            
+            console.log(event.target, 'asdfasdf');
+            console.log($("input:checked + label").text(), 'asldjfalsdf');
+            
+            userAnswers.push($("input:checked + label").text()); //This is the correct way to push the selected value to userAnswers array
             console.log(userAnswers);
+            displayQuestion(questions);
+            // console.log(checkedButton); //How come if I comment this out it jumps to line 72?
+            console.log('asdfhskdjhfkjasdfkjasdf')
+            
+            
+        }
+        if (clickCount == 5) { //if you are on final page and click next or submit
+            
+
+            for (var i = 0; i < arrAnswers.length; i++) {
+                if (arrAnswers[i] === userAnswers[i]) {
+                    numberCorrect += 1
+                } 
+            } finalScore();
+           
         }
     }
     if (event.target.value == "Previous") {
@@ -64,6 +80,8 @@ $('.button').on('click', function (event) {
             // let prevAnswer = userAnswers.pop();
             console.log(userAnswers);
             $('form input[type="radio"]').prop("checked", true);
+            // $('form[name="foo"]').show();
+            // $(".button").show();
         }
     }
 })
@@ -115,4 +133,25 @@ function disableSubmit(submit) {
 function enableButton() {
     // document.getElementById("submit").disabled = false;
     $("#submit").attr("disabled", false);
+}
+
+function finalScore() {
+    console.log('You got', numberCorrect, 'out of 5 correct!')
+    document.getElementById('question').innerText = ('You got' + ' ' + numberCorrect + ' ' + 'out of 5 correct!')
+    // document.getElementById("element").style.display = "none";
+    $('form[name="foo"]').hide();
+    $(".button").hide();
+    // ('You got', numberCorrect, 'out of 5 correct!')
+    var button = document.createElement("button");
+button.innerHTML = "Play Again?";
+
+// 2. Append somewhere
+var body = document.getElementsByTagName("body")[0];
+body.appendChild(button);
+
+// 3. Add event handler
+button.addEventListener ("click", function() {
+    location.reload();
+});
+    
 }
